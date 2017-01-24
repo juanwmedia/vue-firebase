d3d417dd48f0ae0f90406a0e153826b7a7d7a4df
@juanwmedia <juan@wmedia.es>
2017-01-24 18:59:01 +0100

    Renderizado de listas

---
+++ 3-renderizado-de-listas/js/main.js
@@ -0,0 +1,16 @@
+const vm = new Vue({
+    el: 'main',
+    data:  {
+        laborales: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
+        tareas: [
+            {nombre: 'Hacer la compra', prioridad: 'baja'},
+            {nombre: 'Aprender Vue y Firebase', prioridad: 'alta'},
+            {nombre: 'Ir al gimnasio', prioridad: 'alta'},
+        ],
+        persona: {
+            nombre: 'Juan',
+            profesion: 'dev',
+            ciudad: 'Valencia'
+        }
+    },
+});
