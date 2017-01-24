3a1649e4fdca84e2c117de1aeb5a1015924336f0
@juanwmedia <juan@wmedia.es>
2017-01-24 17:23:17 +0100

    1- Instalacion y data binding

---
+++ 1-instalacion-data-binding/js/main.js
@@ -0,0 +1,12 @@
+// Vanilla JavaScript
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
+// input.addEventListener('keyup', () => h1.innerHTML = input.value);
+
+// Vue.js
+new Vue({
+    el: 'main',
+    data: {
+        mensaje: 'Hola mundo!',
+    }
+});
