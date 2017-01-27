7b1dca58ed055d51d14edc0ae84bf64207d6bdc8
@juanwmedia <juan@wmedia.es>
2017-01-27 12:21:40 +0100

    Transiciones

---
+++ 10-transiciones/js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensajes: {
+            transicion: 'Transiciones CSS con Vue.js',
+            animacion: 'Animaciones CSS con Vue.js',
+            animationCustom: 'Animaciones custom CSS con Vue.js',
+            entreElementos: 'Transiciones entre elementos con Vue.js',
+        }
+    },
+});
