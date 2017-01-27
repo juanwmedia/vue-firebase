c0a9474a5f46d2c073ef66254f8b535e928c22e6
@juanwmedia <juan@wmedia.es>
2017-01-27 13:35:07 +0100

    Ajax con Axios

---
+++ 12-ajax-axios/js/main.js
@@ -0,0 +1,23 @@
+Vue.prototype.$http = axios;
+
+new Vue({
+    el: 'main',
+    mounted() {
+        this.cargarPersonas();
+    },
+    data: {
+        personas: []
+    },
+    methods: {
+        cargarPersonas() {
+            this.$http.get('https://randomuser.me/api/?results=500')
+                .then((respuesta) => {
+                    this.personas = respuesta.data.results;
+                });
+            // axios.get('https://randomuser.me/api/?results=500')
+            //     .then((respuesta) => {
+            //         this.personas = respuesta.data.results;
+            //     });
+        }
+    }
+});
