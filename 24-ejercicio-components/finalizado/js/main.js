1f68ceb79d7e206f70904c8c5087612c50b029cd
@juanwmedia <juan@wmedia.es>
2017-02-14 16:50:13 +0100

    Ejercicio componentes

---
+++ 24-ejercicio-components/finalizado/js/main.js
@@ -0,0 +1,39 @@
+Vue.component('usuarios', {
+    props: ['fuente'],
+    template: '#usuarios-template',
+    mounted() {
+        axios.get('https://randomuser.me/api/?results=500')
+            .then((datos) => {
+                const listado = datos.data.results.map((usuario) => {
+                    return {
+                        nombre: `${usuario.name.title}  ${usuario.name.first} ${usuario.name.last}`,
+                        correoe: usuario.email,
+                        foto: usuario.picture.medium,
+                    };
+                });
+                this.usuarios = listado;
+            });
+    },
+    data() {
+        return {
+            usuarios: [],
+            busqueda: '',
+        }
+    },
+    computed: {
+        filtrarUsuarios() {
+            return this.usuarios.filter((usuario) => {
+                return usuario.nombre.includes(this.busqueda)
+            });
+        }
+    }
+});
+
+Vue.component('usuario', {
+    props: ['datos'],
+    template: '#usuario-template',
+});
+
+new Vue({
+    el: 'main',
+});
