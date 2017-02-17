ed06ad0822c6e761a07fc653a1c2f6c353fae037
@juanwmedia <juan@wmedia.es>
2017-02-17 18:30:47 +0100

    Render function

---
+++ 27-render-function/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <elemento tipo="footer">Hola footer</elemento>
+        <elemento tipo="header">Hola header</elemento>
+        <elemento tipo="aside">Hola aside</elemento>
+        <elemento tipo="nav">Hola nav</elemento>
+        <elemento tipo="main">Hola main</elemento>
+    </div>
+
+</template>
+
+<script>
+    import elemento from './components/Elemento.vue';
+    export default {
+        components: { elemento }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 27-render-function/src/components/Elemento.vue
@@ -0,0 +1,35 @@
+<!--<template>
+    <div>
+        <article v-if="tipo === 'article'">
+            <slot></slot>
+        </article>
+        <section v-if="tipo === 'section'">
+            <slot></slot>
+        </section>
+        <aside v-if="tipo === 'aside'">
+            <slot></slot>
+        </aside>
+        <nav v-if="tipo === 'nav'">
+            <slot></slot>
+        </nav>
+        <header v-if="tipo === 'header'">
+            <slot></slot>
+        </header>
+        <footer v-if="tipo === 'footer'">
+            <slot></slot>
+        </footer>
+    </div>
+</template>-->
+<script>
+    export default {
+        props: ['tipo'],
+        render(createElement) {
+            return createElement(
+                this.tipo,
+                this.$slots.default,
+            )
+        }
+    }
+</script>
+<style>
+</style>
