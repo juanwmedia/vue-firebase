f3c387459ef41cc346af0f33f6f0942a32c28c8b
@juanwmedia <juan@wmedia.es>
2017-02-20 13:21:58 +0100

    Hot Reloading

--- 27-render-function/src/components/Elemento.vue
+++ 27-render-function/src/components/Elemento.vue
@@ -32,4 +32,4 @@
     }
 </script>
 <style>
-</style>
+</style>c
---
+++ 28-hot-reloading/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <tareas></tareas>
+</template>
+
+<script>
+    import Tareas from './components/Tareas.vue';
+    export default {
+        components: {Tareas},
+    }
+</script>
