cd752503444951a4e18f98925a58b01b44a1e265
@juanwmedia <juan@wmedia.es>
2017-02-21 18:22:43 +0100

    Scoped CSS

---
+++ 29-scoped-css/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Menú principal</h1>
+        <input type="range">
+        <ul>
+            <li><a href="">Enlace 1</a></li>
+            <li><a href="">Enlace 2</a></li>
+            <li><a href="">Enlace 3</a></li>
+            <li><a href="">Enlace 4</a></li>
+            <li><a href="">Enlace 5</a></li>
+            <li><a href="">Enlace 6</a></li>
+        </ul>
+        <tareas></tareas>
+    </div>
+</template>
+
+<script>
+    import Tareas from './components/Tareas.vue';
+    export default {
+        components: {Tareas}
+    }
+</script>
---
+++ 29-scoped-css/src/components/Tareas.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h2>Las Tareas por hacer</h2>
+        <ul>
+            <li v-for="tarea in tareasPendientes">
+                <a href="#" @click="actualizarTarea(tarea)" v-text="tarea.nombre"></a>
+            </li>
+        </ul>
+        <h2>Tareas finalizadas</h2>
+        <li v-for="tarea in tareasFinalizadas">
+            <a href="#" @click="actualizarTarea(tarea)" v-text="tarea.nombre"></a>
+        </li>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                tareas: [
+                    {nombre: 'Aprender Vue.js', completado: false},
+                    {nombre: 'Grabar el módulo de Vuex', completado: false},
+                    {nombre: 'Responder comentarios (de una vez)', completado: false},
+                    {nombre: 'Salir a correr para despejarme', completado: false},
+                    {nombre: 'Diseñar el módulo de Firebase', completado: false},
+                    {nombre: 'Diseñar el ejercicio final', completado: false},
+                    {nombre: 'Tomarme unas vacaciones', completado: false},
+                ],
+                finalizadas: []
+            }
+        },
+        methods: {
+            actualizarTarea(tarea) {
+                tarea.completado = !tarea.completado;
+            }
+        },
+        computed: {
+            tareasPendientes() {
+                return this.tareas.filter((tarea) => !tarea.completado);
+            },
+            tareasFinalizadas() {
+                return this.tareas.filter((tarea) => tarea.completado);
+            }
+        }
+    }
+</script>
+<style scoped>
+    li a {
+        color: red;
+    }
+</style>
