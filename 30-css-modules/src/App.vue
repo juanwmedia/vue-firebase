a14547fa6d1a1a474ab65db364a3a1ea82451682
@juanwmedia <juan@wmedia.es>
2017-02-22 14:36:13 +0100

    CSS Modules

---
+++ 30-css-modules/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!--<h1 :class="[$style.rojo]">Hola CSS Modules</h1>-->
+    <h1 :class="{[$style.subrayado] : subrayado}">Hola CSS Modules</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                subrayado: false
+            }
+        }
+    }
+</script>
+
+<style module>
+    .subrayado {
+        text-decoration: underline;
+    }
+    .rojo {
+        color: red;
+    }
+</style>
