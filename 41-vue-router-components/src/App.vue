cf2f08c9d12645b420f9577ebd5e19832f732117
@juanwmedia <juan@wmedia.es>
2017-03-09 16:42:53 +0100

    Vue Router with components

---
+++ 41-vue-router-components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <h1>Rutas con Vue Router</h1>
+        <router-link to="/">Home</router-link>
+        <router-link to="/contacto">Contacto</router-link>
+        <hr>
+        <router-view></router-view>
+    </section>
+</template>
+
+<script>
+    export default {
+    }
+</script>
---
+++ 41-vue-router-components/src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import Contacto from './components/Contacto.vue';
+
+export const routes = [
+    {path: '/', component: Home},
+    {path: '/contacto', component: Contacto},
+];
