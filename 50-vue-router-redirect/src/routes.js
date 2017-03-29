324acafc9957553704db6eb343998cad8184fc5e
@juanwmedia <juan@wmedia.es>
2017-03-29 17:09:11 +0200

    Vue Router redirect & alias

---
+++ 50-vue-router-redirect/src/routes.js
@@ -0,0 +1,17 @@
+import Home from './components/Home.vue';
+import Equipo from './components/Equipo.vue';
+import Usuario from './components/Usuario.vue';
+import UsuarioFotos from './components/UsuarioFotos.vue';
+import UsuarioBio from './components/UsuarioBio.vue';
+
+export const routes = [
+    {path: '/', component: Home, name: 'home'},
+    {path: '/prueba', alias: '/otraprueba', redirect: {name: 'home'},  component: Home},
+    {path: '/equipo/:id', component: Equipo, children: [
+        {path: '', components: {
+            default: Usuario,
+            bio: UsuarioBio,
+            fotos: UsuarioFotos,
+        }, name:'equipo'},
+    ]},
+];
