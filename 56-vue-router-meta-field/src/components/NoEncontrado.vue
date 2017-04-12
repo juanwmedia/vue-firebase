4925bc964181f31bcafa53685eae04b060e6b010
@juanwmedia <juan@wmedia.es>
2017-04-12 20:55:58 +0200

    Vue Router meta fields

---
+++ 56-vue-router-meta-field/src/components/NoEncontrado.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>No encuentro: 404</h2>
+</template>
---
+++ 56-vue-router-meta-field/src/components/Usuario.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <h1>El identificador es: {{ $route.params.id }}</h1>
+        <h2>Juan Andrés Núñez</h2>
+        <h5>juan@wmedia.es</h5>
+    </section>
+</template>
+
+<script>
+    export default {
+        beforeRouteEnter: ((to, from, next) => {
+            console.info('Antes de entrar');
+            next((vm) => console.log(vm));
+        }),
+        beforeRouteUpdate: ((to, from, next) => {
+           console.info('Antes de cambiar la ruta sobre el mismo componente');
+           next();
+        }),
+        beforeRouteLeave: ((to, from, next) => {
+           console.info('Antes de salir');
+           next();
+        }),
+    }
+</script>
+
+
