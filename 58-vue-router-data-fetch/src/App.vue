42b76c320cfaff2536ecd491cd3bfaf33bb5b70c
@juanwmedia <juan@wmedia.es>
2017-04-25 14:20:28 +0200

    Vue Router data fetch after

---
+++ 58-vue-router-data-fetch/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <button @click="cambiar">Cambiar Auth</button>
+        <hr>
+        <transition name="aparecer" appear>
+            <router-view></router-view>
+        </transition>
+    </section>
+</template>
+
+<script>
+    import {mapMutations} from 'vuex';
+    export default {
+        methods: mapMutations(['cambiar']),
+    }
+</script>
+<style>
+    /* Transition */
+    .aparecer-enter {
+        opacity: 0;
+    }
+
+    .aparecer-enter-active {
+        transition: opacity 1s;
+    }
+
+    .aparecer-leave-to {
+        opacity: 0;
+    }
+
+    .aparecer-leave-active {
+        transition: opacity 1s;
+    }
+</style>
---
+++ 58-vue-router-data-fetch/src/components/Usuario.vue
@@ -0,0 +1,45 @@
+<template>
+    <section>
+        <h1>El identificador es: {{ $route.params.id }}</h1>
+        <h2>Juan Andrés Núñez</h2>
+        <h5>juan@wmedia.es</h5>
+        <pre v-text="username"></pre>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: null
+            }
+        },
+        created() {
+            this.obtenerUsername();
+        },
+        watch: {
+          '$route': 'obtenerUsername'
+        },
+        methods: {
+            obtenerUsername() {
+                setTimeout(() => {
+                    this.username = this.$route.params.id + Math.floor(Math.random() * 1000);
+                }, 2000);
+            }
+        },
+        beforeRouteEnter: ((to, from, next) => {
+            console.info('Antes de entrar');
+            next((vm) => console.log(vm));
+        }),
+        beforeRouteUpdate: ((to, from, next) => {
+            console.info('Antes de cambiar la ruta sobre el mismo componente');
+            next();
+        }),
+        beforeRouteLeave: ((to, from, next) => {
+            console.info('Antes de salir');
+            next();
+        }),
+    }
+</script>
+
+
